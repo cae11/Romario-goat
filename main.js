bfe16a217065f72491c974a3994e4688b48c8caf
cae11 <163418269+cae11@users.noreply.github.com>
2024-03-15 10:06:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://conteudo.imguol.com.br/c/esporte/ec/2021/09/10/romario-em-atuacao-pelo-barcelona-1631302145066_v2_450x600.jpg"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://s3.amazonaws.com/content.espn.com.br/image/livre/74102cfb-76a4-37af-8209-5b47a675a562.jpg"
+}
